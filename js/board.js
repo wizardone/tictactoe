3dfc5f026093db6b7787d084af71eb42d07a3af3
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2016-02-09 15:04:58 +0200

    add winning combinations

--- js/board.js
+++ js/board.js
@@ -36,7 +36,10 @@ var Board = React.createClass({
       players: [_player1, _player2],
       freeIndexes: [0,1,2,3,4,5,6,7,8],
       totalTurns: 0,
-      currentTurn: 0
+      currentTurn: 0,
+      winningCombos: [[0,1,2], [3,4,5], [6,7,8],
+                      [0,3,6], [1,4,7], [2,5,8],
+                      [0,4,8], [2,4,6]]
     }
   },
 
